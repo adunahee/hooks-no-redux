913526993f9c8a1a1d5853239b4ec52cbbc52d8a
Anthony D <dunahee@gmail.com>
2019-03-29 14:23:05 -0500

    use state handles an array that is updated on clicking enter

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
-import React, {useState, useReducer, useEffect, useContext} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useReducer, useEffect, useContext } from 'react';
 
 const App = () => {
-    const [civ, setCiv ] = useState('');
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <label>
-            Enter a civilization:
+  //creates a state var with default value of empty string
+  const [civ, setCiv] = useState('');
+  const [civArr, setCivArr] = useState([]);
+  return (
+    <div>
+      <header>
+        <h1>Civilizations of the World</h1>
+      </header>
+      <body>
+
+        <section>
+          <h2>Civilization Form</h2>
+          <form>
+            <label>
+              Enter a civilization:
             <input type="text" value={civ} onChange={(event) => setCiv(event.target.value)} />
-          </label>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+            </label>
+            <button type='submit' onClick={(e) => {
+              e.preventDefault();
+              setCivArr([...civArr, civ])
+            }}>Add Civilization</button>
+          </form>
+        </section>
+
+        <section>
+          <h2>Civilization Summary</h2>
+          <ul>
+            {civArr.map((c, i) => (<li key={i} >{c}</li>))}
+          </ul>
+        </section>
+
+      </body>
+      <footer>
+        Fun with Hooks
+        </footer>
+    </div>
+  );
+}
 
 export default App;
